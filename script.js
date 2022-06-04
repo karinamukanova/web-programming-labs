81b9370ec2c7e0a9efc49f3438adc8905ee5322f
Karina <karinochka.mykanova@gmail.com>
2022-06-04 21:49:59 +0700

    Генерация меню

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function notReadyAlert() {
-    alert('Sorry, not ready yet! \n Извините, еще не готово!');
+    alert('Sorry, not ready yet!\nИзвините, еще не готово!');
     return false;
 }
 
 function search() {
     let name = document.getElementById('search').value;
     let productNumber = null;
-    if (name=='RELX') {
-    productNumber = 0;
-    } else if (name == 'Eleaf') {
-    productNumber = 1;
-    } else if (name == 'MYBLU') {
-    productNumber = 2;
+    if (name=='Мышь') {
+        productNumber = 0;
+    } else if (name == 'Монитор') {
+        productNumber = 1;
+    } else if (name == 'Клавиатура') {
+        productNumber = 2;
     } else {
-    alert ('Товар не найден');
+        alert ('Товар не найден');
     }
     console.log('Товар номер:', productNumber);
     let cards = document.getElementsByClassName('card');
@@ -22,16 +22,47 @@ function search() {
     card.style.backgroundColor = 'yellow';
 
     setTimeout(function() {
-    card.style.border = 'none';
-    card.style.backgroundColor = '';
+        card.style.border = 'none';
+        card.style.backgroundColor = '';
     }, 2000);
+}
+
+function generateMenu() {
+    let menu = document.querySelector('nav.main-menu ul');
+    menu.innerHTML = '';
+
+    let items = [
+        {href: 'index.html', text: 'Товар'},
+        {href: '', text: 'Контакты'},
+        {href: '', text: 'Доставка'},
+        {href: '', text: 'Акции'},
+        {href: '', text: 'О нас'},
+    ];
+
+    for(let i = 0; i<items.length; i++) {
+        let link = document.createElement('a');
+        link.innerText = items[i].text;
+        link.href = items[i].href;
+        if(items[i].href == '') {
+            link.addEventListener('click', notReadyAlert);
+        }
+
+        let menuItem = document.createElement('li');
+        menuItem.appendChild(link);
+
+        menu.appendChild(menuItem);
+    }
+
 }
 
 function loaded() {
     let searchbox = document.getElementById('search');
     searchbox.addEventListener('keydown', function (key) {
-        if (key.key == 'Enter') 
+        if (key.key == 'Enter') {
             search();
-        });
+        }
 
+    });
+
+    generateMenu();
 }
